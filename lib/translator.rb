99fee2c6173ca673b6f7db0ea17d2096246d8942
Tawhid Ali <tawhidali126@gmail.com>
2019-08-16 23:25:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,6 +35,6 @@ def get_japanese_emoticon(file_path, emoticon)
   
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  english_hash = load_library(file_path)
 end
